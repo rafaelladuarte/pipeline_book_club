c00b78de44f5e338aec21fa010b524065eb5d318
elladarte <rafaella.d.d.carvalho@gmail.com>
2023-03-30 11:05:16 -0300

    start dev scraper

---
+++ extraction/scraper.py
@@ -0,0 +1,38 @@
+import requests
+import re
+
+class Scraper:
+    def __init__(self):
+        pass
+
+    def get_list_category(self):
+        pass
+
+    def pagination(self):
+        pass
+
+    def get_list_books(self):
+        pass
+
+    def get_book(self):
+        pass
+
+    def _get_name(self):
+        pass
+
+    def _get_price(self):
+        pass
+
+    def _get_stok(self):
+        pass
+
+    def _get_available(self):
+        pass
+
+    def _get_image(self):
+        pass
+
+    def _get_raiting(self):
+        pass
+
+    
---
+++ extraction/test.py
@@ -0,0 +1,3 @@
+from scraper import Scraper
+
+home_url = 'http://books.toscrape.com/index.html'
